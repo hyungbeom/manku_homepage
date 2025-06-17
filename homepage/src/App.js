ccea73cc1839d5c1668b66df4120778c208f8206
progist <hblee@progist.co.kr>
2025-06-18 02:36:06 +0900

    만쿠홈페이지

---
+++ homepage/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Main from "./page/Main";
+import './index.css'
+function App() {
+
+    return (
+        <>
+            <Header/>
+
+            <Main/>
+        </>
+    );
+}
+
+export default App;
